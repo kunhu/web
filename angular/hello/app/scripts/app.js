0a773a8c4d0266a6dd9752ef404d679d491f857a
kunhu.tsai <kunhu.tsai@gmail.com>
2018-06-15 10:15:45 +0000

    [Angular] init okay change to port to 12345 change ip 0.0.0.0

---
+++ angular/hello/app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name helloApp
+ * @description
+ * # helloApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('helloApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ angular/hello/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name helloApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the helloApp
+ */
+angular.module('helloApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
